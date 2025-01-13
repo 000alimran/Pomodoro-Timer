352f43e1e1d9723cdfffea78aa5eb1376178e9b7
000alimran <000alimran@gmail.com>
2025-01-14 01:06:35 +0600

    Beep Sound Added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Add beep sound
+const beepSound = new Audio('beep.mp3');
+
+// Function to play sound
+const playBeep = () => {
+  beepSound.currentTime = 0; // Reset the audio to start
+  beepSound.play(); // Play the beep sound
+};
+
 let timer;
 let isRunning = false;
 let minutes = 25;
@@ -11,6 +20,7 @@ const updateTimerDisplay = () => {
 const startTimer = () => {
   if (!isRunning) {
     isRunning = true;
+    playBeep(); // Play beep sound on start
     timer = setInterval(() => {
       if (seconds === 0) {
         if (minutes === 0) {
@@ -31,6 +41,7 @@ const startTimer = () => {
 const pauseTimer = () => {
   clearInterval(timer);
   isRunning = false;
+  playBeep(); // Play beep sound on pause
 };
 
 const resetTimer = () => {
@@ -38,6 +49,7 @@ const resetTimer = () => {
   isRunning = false;
   minutes = 25;
   seconds = 0;
+  playBeep(); // Play beep sound on reset
   updateTimerDisplay();
 };
 
